1395d572ac8d22136bab630dad05a54c191dee59
mcgnb <161626140+mcgnb@users.noreply.github.com>
2025-03-11 18:22:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,12 +11,14 @@ else
     PYTHON_SCRIPT=""
 
     # Déterminer le script Python en fonction de l'argument
-    if [ "$ARGUMENT" == "trix_multi_bitmart" ]; then
-        PYTHON_SCRIPT="python3 Live-Tools-V2/strategies/trix/multi_bitmart.py"
-    elif [ "$ARGUMENT" == "trix_multi_bitmart_lite" ]; then
-        PYTHON_SCRIPT="python3 Live-Tools-V2/strategies/trix/multi_bitmart_lite.py"
-    elif [ "$ARGUMENT" == "envelopes_multi_bitget" ]; then
-        PYTHON_SCRIPT="python3 Live-Tools-V2/strategies/envelopes/multi_bitget.py"
+    if [ "$ARGUMENT" == "trix_long" ]; then
+        PYTHON_SCRIPT="python3 Live-Tools/strategies/trix/trix_long.py"
+    elif [ "$ARGUMENT" == "trix_short" ]; then
+        PYTHON_SCRIPT="python3 Live-Tools/strategies/trix/trix_short.py"
+    elif [ "$ARGUMENT" == "mean_reversion" ]; then
+        PYTHON_SCRIPT="python3 Live-Tools/strategies/mean_reversion/multi_hl.py"
+    elif [ "$ARGUMENT" == "ichimoku_scalper" ]; then
+        PYTHON_SCRIPT="python3 Live-Tools/strategies/ichimoku_scalp/ichi_scalp_strat.py"
     else
         echo "Argument non reconnu. Aucun ajout ne sera effectué."
     fi
@@ -24,11 +26,11 @@ else
     # Si un script Python a été défini, procéder à l'ajout
     if [ -n "$PYTHON_SCRIPT" ]; then
         # Vérifier si la ligne existe déjà dans 1hcron.sh
-        if grep -Fxq "$PYTHON_SCRIPT" Live-Tools-V2/1hcron.sh; then
+        if grep -Fxq "$PYTHON_SCRIPT" Live-Tools/1hcron.sh; then
             echo "Le script $PYTHON_SCRIPT existe déjà dans 1hcron.sh"
         else
             # Ajouter la ligne au fichier 1hcron.sh
-            echo "$PYTHON_SCRIPT" >> Live-Tools-V2/1hcron.sh
+            echo "$PYTHON_SCRIPT" >> Live-Tools/1hcron.sh
             echo "Le script $PYTHON_SCRIPT a été ajouté à 1hcron.sh"
         fi
     fi
@@ -44,7 +46,7 @@ sudo apt install pip -y
 touch cronlog.log
 
 echo "Installation des packages nécessaires..."
-cd Live-Tools-V2
+cd Live-Tools
 sudo apt-get install python3-venv -y
 python3 -m venv .venv
 source .venv/bin/activate
@@ -53,10 +55,10 @@ git update-index --assume-unchanged secret.py
 cd ..
 
 # Ajouter la tâche cron si elle n'existe pas déjà
-crontab -l | grep -q 'bash ./Live-Tools-V2/1hcron.sh'
+crontab -l | grep -q 'bash ./Live-Tools/1hcron.sh'
 if [ $? -ne 0 ]; then
     # Ajouter la tâche cron
-    (crontab -l 2>/dev/null; echo "0 * * * * /bin/bash ./Live-Tools-V2/1hcron.sh >> cronlog.log") | crontab -
+    (crontab -l 2>/dev/null; echo "0 * * * * /bin/bash ./Live-Tools/1hcron.sh >> cronlog.log") | crontab -
     echo "Tâche cron ajoutée avec succès."
 else
     echo "La tâche cron existe déjà."
